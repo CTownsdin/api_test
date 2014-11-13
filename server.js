4a053ac87a3932c91c6d592150653ae6b695900d
Ivan Storck <ivan@ivanstorck.com>
2014-11-13 05:21:39 +0000

    change to /current route that takes a city

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express');
 var app = express();
 var request = require('superagent');
 
-app.get('/', function (req, res) {
+app.get('/current/:city', function (req, res) {
     var wunderUrl = 'http://api.wunderground.com/api/' +
     process.env.WUNDERAPI +
     '/conditions/q/WA/' +
-    'Seattle' +
+    req.params.city +
     '.json';
 
     request
@@ -20,6 +20,8 @@ app.get('/', function (req, res) {
 });
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), function() {
-  console.log('server running on port: ' + app.get('port'));
+var server = app.listen(app.get('port'), function() {
+  var host = process.env.IP;
+  var port = server.address().port;
+  console.log('wunderground proxy server listening at http://%s:%s', host, port); 
 });
